7e795023bf85aab04099d3a9d6766580bc1f5045
boybeak <boybeak@gmail.com>
2023-07-22 16:01:56 +0800

    feat: add @Inherited to V8Field and V8Method

--- v8binding/src/main/java/com/github/boybeak/v8x/binding/annotation/V8Field.kt
+++ v8binding/src/main/java/com/github/boybeak/v8x/binding/annotation/V8Field.kt
@@ -1,5 +1,8 @@
 package com.github.boybeak.v8x.binding.annotation
 
+import java.lang.annotation.Inherited
+
 @Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.RUNTIME)
+@Inherited
 annotation class V8Field(val binding: Boolean = false)
--- v8binding/src/main/java/com/github/boybeak/v8x/binding/annotation/V8Method.kt
+++ v8binding/src/main/java/com/github/boybeak/v8x/binding/annotation/V8Method.kt
@@ -1,5 +1,8 @@
 package com.github.boybeak.v8x.binding.annotation
 
+import java.lang.annotation.Inherited
+
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
+@Inherited
 annotation class V8Method(val jsFuncName: String = "")
